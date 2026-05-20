e48f8cbcfea2d05194aca9b7b001b4e1d2be1fa4
ISABELLE SANTOS DA CRUZ <isabelle.6973@aluno.pr.senac.br>
2026-05-20 17:13:10 -0300

    Terminando de colocar componentes de visualização e aplicando vector icons do expo no projeto

--- App.tsx
+++ App.tsx
@@ -1,36 +1,59 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity, 
+import {
+  View, Text, Image, StyleSheet, TouchableOpacity,
   SafeAreaView, StatusBar
 } from 'react-native';
+
+import { MaterialCommunityIcons, Feather } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <SafeAreaView>
       <StatusBar />
       <View>
-<View> 
-  <Image/>
-  <Text>Mc Donald's</Text>
-  </View>
-<View>
-  <Text>Seja Bem-Vindo!</Text>
-  <Text>
-    Escolha como prefere aproveitar sua refeição. Estamos aqui para oferecer praticidade 
-    e sabor em cada detalhe!
-  </Text>
-      </View>
+        <View>
+          <Image />
+          <Text>Mc Donald's</Text>
+        </View>
+        <View>
+          <Text>Seja Bem-Vindo!</Text>
+          <Text>
+            Escolha como prefere aproveitar sua refeição. Estamos aqui para oferecer praticidade
+            e sabor em cada detalhe!
+          </Text>
+        </View>
+        <View>
+          <TouchableOpacity>
+            <View>
+              <MaterialCommunityIcons
+              name="hamburger"
+              />
+            </View>
+            <View>
+              <Text>Para comer aqui!</Text>
+            </View>
+            </TouchableOpacity>
+          <TouchableOpacity>
+            <View>
+              <Feather name="shopping-bag"/>
+            </View>
+            <View>
+              <Text>Para levar!</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
- safeArea: {
+  safeArea: {
 
- },
- container: {
+  },
+  container: {
 
   },
   header: {
-    
+
   },
   logo: {
 
@@ -39,25 +62,25 @@ const styles = StyleSheet.create({
 
   },
   welcomeSection: {
-    
+
   },
   title: {
 
   },
   subtitle: {
-    
+
   },
   cardsRow: {
 
   },
   card: {
-    
+
   },
   iconCircle: {
 
   },
   cardLabelPill: {
-    
+
   },
   cardLabel: {
 
